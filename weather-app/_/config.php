2640f7b88cd9800bd9cd124023fc96ab9abcbcd9
Nirjon Roy <roynirjon18@gmail.com>
2022-12-12 13:35:05 +0600

    update js projects

---
+++ weather-app/_/config.php
@@ -0,0 +1,27 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "itnaijra_world";
+// MySQL Database Password
+$sql_db_pass = "Itna12345@@@";
+// MySQL Database Name
+$sql_db_name = "itnaijra_world";
+
+// Site URL
+$site_url = "https://aemalworld.com"; // e.g (http://example.com)
+
+$auto_redirect = true;
+
+// Purchase code
+$purchase_code = "01bde571-c269-4fd8-8d7b-4e4cd76e7902"; // Your purchase code, don't give it to anyone.
+?>
